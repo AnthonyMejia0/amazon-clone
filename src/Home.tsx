91e9755d9c564891bbd294f4e07e3621ed830498
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-28 16:51:36 -0700

    added more products

--- src/Home.tsx
+++ src/Home.tsx
@@ -25,6 +25,37 @@ function Home() {
                   img='https://i.imgur.com/PAKm2hK.jpg'
                 />
             </div>
+
+            <div className='flex z-[1] mx-[5px]'>
+                <Product 
+                  title='CORSAIR K55 RGB PRO-Dynamic RGB Backlighting - Six Macro Keys with Elgato Stream Deck Software Integration-IP42 Dust and Spill Resistant-Detachable Palm Rest-Dedicated Media and Volume Keys, Black'
+                  price={49.99}
+                  rating={4}
+                  img='https://i.imgur.com/cDR0ooH.jpg'
+                />
+                <Product 
+                  title='Echo Dot (3rd Gen, 2018 release) - Our most popular smart speaker with Bluetooth and Alexa - Charcoal'
+                  price={29.99}
+                  rating={4}
+                  img='https://i.imgur.com/ttoIP9X.jpg'
+                />
+                <Product 
+                  title='iHealth COVID-19 Antigen Rapid Test, 2 Tests per Pack,FDA EUA Authorized OTC At-home Self Test, Results in 15 Minutes with Non-invasive Nasal Swab, Easy to Use & No Discomfort '
+                  price={17.08}
+                  rating={4}
+                  img='https://i.imgur.com/Auwgt1Q.jpg'
+                />
+            </div>
+
+            <div className='flex z-[1] mx-[5px]'>
+                <Product 
+                  title='
+                  RitFit Garage & Home Gym Package Includes Optional 1000LBS Power Cage with LAT Pull Down,Weight Bench, Barbell Set with Olympic Barbell'
+                  price={1299.99}
+                  rating={4}
+                  img='https://i.imgur.com/OJXh6mW.jpg'
+                />
+            </div>
         </div>
     </div>
   )
