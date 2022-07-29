d5bf914a55cb3e6e741bbc3b4b01ac8784856407
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-29 06:08:05 -0700

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import Header from './Header';
-import Home from './Home';
+import Header from './components/Header';
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from './components/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+        <Header />
+        <Routes>
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+    </Router>
   );
 }
 
