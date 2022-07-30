7a069ad259b97d787af417488b8ff2617eed0047
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-29 20:38:36 -0700

    created cart item component

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,59 @@
+import { useRecoilState } from "recoil";
+import { cartItem, cartState } from "../atoms/cartAtom";
+
+type Props = {
+  id: number;
+  title: string;
+  price: number;
+  rating: number;
+  img: string;
+};
+
+function CartItem({ id, title, price, rating, img }: Props) {
+  const [cart, setCart] = useRecoilState(cartState);
+
+  const removeFromCheckout = () => {
+    let newCart = [] as cartItem[];
+    for (let i: number = 0; i < cart.length; i += 1) {
+      if (i !== id) {
+        newCart.push(cart[i]);
+      }
+    }
+    setCart(newCart);
+  };
+
+  return (
+    <div className="flex my-[20px]">
+      <img
+        className="h-[180px] w-[180px] object-contain"
+        src={img}
+        alt="Cart item"
+      />
+
+      <div className="pl-[20px]">
+        <p className="text-[17px] font-extrabold">{title}</p>
+        <p className="flex items-center space-x-[1px]">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+
+        <div className="flex">
+          {Array(rating)
+            .fill(0, 0, rating)
+            .map((_, i) => (
+              <p key={i}>⭐</p>
+            ))}
+        </div>
+
+        <button
+          onClick={removeFromCheckout}
+          className="bg-[#f0c14b] border b-color text-[#111] mt-[10px] px-1 rounded-sm"
+        >
+          Remove from cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
