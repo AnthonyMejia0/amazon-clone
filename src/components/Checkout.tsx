c4fe57168f979b81fe9ecf2a6640f66c91576baf
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-29 20:38:48 -0700

    added cart items to checkout

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,27 +1,44 @@
-import { useRecoilValue } from "recoil"
-import { cartState } from "../atoms/cartAtom"
-import Subtotal from "./Subtotal"
+import { useRecoilState } from "recoil";
+import { cartState } from "../atoms/cartAtom";
+import CartItem from "./CartItem";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
-  const cart = useRecoilValue(cartState);
+  const [cart, setCart] = useRecoilState(cartState);
 
   return (
     <div className="flex p-[20px] bg-white h-[max-content]">
+      <div>
+        <img
+          className="w-full mb-[10px]"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt="Sample"
+        />
         <div>
-            <img className="w-full mb-[10px]" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="Sample" />
-            <div>
-                <h2 className="mr-[10px] p-[10px] border-b border-b-gray-400">Your Shopping Cart</h2>
-                {cart.map((item, i) => (
-                  <p key={i}>{item?.title}</p>
-                ))}
-            </div>
+          <h2 className="mr-[10px] p-[10px] border-b border-b-gray-400">
+            Your Shopping Cart
+          </h2>
         </div>
 
         <div>
-            <Subtotal />
+          {cart?.map((item, i) => (
+            <CartItem
+              key={i}
+              id={i}
+              title={item.title}
+              price={item.price}
+              rating={item.rating}
+              img={item.img}
+            />
+          ))}
         </div>
+      </div>
+
+      <div>
+        <Subtotal />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
