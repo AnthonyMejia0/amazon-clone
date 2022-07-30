57387977c7b0ca825b33606ac7cf3401b8bf28b1
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-29 17:16:44 -0700

    add to cart functionality

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,6 @@
+import { useRecoilState } from "recoil";
+import { cartState } from "../atoms/cartAtom";
+
 type Props = {
     title: string;
     price: number;
@@ -6,26 +9,37 @@ type Props = {
 }
 
 function Product({ title, price, rating, img }: Props) {
-  return (
+    const [cart, setCart] = useRecoilState(cartState);
+    
+    const addToCart = () => {
+        setCart([...cart, {
+            title: title,
+            price: price,
+            rating: rating,
+            img: img
+        }]);
+    }
+
+    return (
     <div className='relative flex flex-col items-center justify-end m-[10px] p-[20px] w-full max-h-[400px] min-w-[100px] bg-white z-[1]'>
-        <div className='h-[100px] mb-[15px]'>
-            <p>{title}</p>
+        <div className='h-[100px] mb-[25px]'>
+            <p className='line-clamp-3'>{title}</p>
             <p className='mt-[5px]'>
                 <small>$</small>
                 <strong>{price}</strong>
             </p>
             <div className='flex'>
                 {Array(rating).fill(0, 0, rating).map((_, i) => (
-                    <p>⭐</p>
+                    <p key={i}>⭐</p>
                 ))}
             </div>
         </div>
         <img 
-            className='max-h-[150px] w-full object-contain mb-[15px]' 
+            className='max-h-[150px] min-h-[150px] w-full object-contain mb-[15px]' 
             src={img}
             alt={title}
         />
-        <button className='bg-[#f0c14b] border b-color text-[#111] mt-[10px] px-1 rounded-sm'>Add To Cart</button>
+        <button onClick={addToCart} className='bg-[#f0c14b] border b-color text-[#111] mt-[10px] px-1 rounded-sm'>Add To Cart</button>
     </div>
   )
 }
