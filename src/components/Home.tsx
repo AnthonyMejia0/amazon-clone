5ff8a21dece8cd9f1b1286113b07ef6431c5066a
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-31 05:12:54 -0700

    removed spacing

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,11 +4,13 @@ function Home() {
   return (
     <div className="flex justify-center mx-auto max-w-[1500px]">
       <div>
-        <img
-          className="w-full z-[-1] mb-[-150px] mask"
-          src="https://i.imgur.com/WERPlqW.jpg"
-          alt="Banner"
-        />
+        <div>
+          <img
+            className="w-full z-[-1] mb-[-150px] mask"
+            src="https://i.imgur.com/WERPlqW.jpg"
+            alt="Banner"
+          />
+        </div>
 
         <div className="flex z-[1] mx-[5px]">
           <Product
