64e214731b7d276c8c89f556c4612eeaca269bcd
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-08-06 16:27:22 -0700

    created orders page

---
+++ src/components/Order.tsx
@@ -0,0 +1,38 @@
+import moment from "moment";
+import CurrencyFormat from "react-currency-format";
+import CartItem from "./CartItem";
+
+function Order({ order }) {
+  return (
+    <div className="p-[40px] my-[20px] border border-gray-300 bg-white relative">
+      <h2 className="text-2xl font-extrabold">Order</h2>
+      <p>{moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}</p>
+      <p className="absolute top-[40px] right-[20px]">
+        <small>{order.id}</small>
+      </p>
+      {order.data.cart?.map((item, i) => (
+        <CartItem
+          key={i}
+          id={item.id}
+          title={item.title}
+          price={item.price}
+          rating={item.rating}
+          img={item.img}
+          buttonDisabled={true}
+        />
+      ))}
+      <CurrencyFormat
+        renderText={(value: string) => (
+          <h3 className="text-lg font-bold text-right">Order Total: {value}</h3>
+        )}
+        decimalScale={2}
+        value={order.data.amount / 100}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+    </div>
+  );
+}
+
+export default Order;
---
+++ src/components/Orders.tsx
@@ -0,0 +1,42 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { userState } from "../atoms/userAtom";
+import { db } from "../config/config";
+import Order from "./Order";
+
+function Orders() {
+  const user = useRecoilValue(userState);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      const orderRef = collection(db, "users", user?.uid, "orders");
+      const q = query(orderRef, orderBy("created", "desc"));
+      onSnapshot(q, (snapshot) =>
+        setOrders(
+          snapshot.docs.map((d) => ({
+            id: d.id,
+            data: d.data(),
+          }))
+        )
+      );
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+
+  return (
+    <div className="py-[20px] px-[80px]">
+      <h1 className="text-4xl font-extrabold">Your Orders</h1>
+
+      <div className="my-[30px]">
+        {orders?.map((order, i) => (
+          <Order key={i} order={order} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
